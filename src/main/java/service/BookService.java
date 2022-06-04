918ea536f06d69bee18c8c2c0687f76bc8035665
Tatiana Komarova <tatiana78977ua@gmail.com>
2022-06-04 20:24:42 +0300

    library: classes were added

---
+++ src/main/java/service/BookService.java
@@ -0,0 +1,50 @@
+package service;
+
+import enums.Message;
+import model.Book;
+import repository.BookRepository;
+
+import java.util.List;
+import java.util.Objects;
+
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(String path) {
+        this.bookRepository = new BookRepository(path);
+    }
+
+    public List<Book> getAll() {
+        return bookRepository.getAll();
+    }
+
+    public void save(Book book) {
+        var isPresent = bookRepository.getAll().stream()
+                .anyMatch(b -> areSame(b, book));
+        if (!isPresent) {
+            bookRepository.create(book);
+            System.out.println(Message.ADD.getValue());
+        } else {
+            System.out.println(Message.NOT_ADD.getValue());
+        }
+    }
+
+    public void delete(String nameOfBook) {
+        var isPresent = bookRepository.getAll().stream()
+                .anyMatch(b -> b.getName().equals(nameOfBook));
+
+        if (isPresent) {
+            bookRepository.delete(nameOfBook);
+            System.out.println(Message.DELETE.getValue());
+        } else {
+            System.out.println(Message.NOT_EXISTS.getValue());
+        }
+    }
+
+    private boolean areSame(Book b1, Book b2) {
+        return Objects.equals(b1.getName(), b2.getName()) &&
+                Objects.equals(b1.getAuthor(), b2.getAuthor()) &&
+                Objects.equals(b1.getDescription(), b2.getDescription()) &&
+                Objects.equals(b1.getNumberOfPages(), b2.getNumberOfPages());
+    }
+}
